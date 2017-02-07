4b1348c99b90e3b293c759a30a79cececc27cbeb
levg34 <guilhem.dross@laposte.net>
2017-02-07 16:05:30 +0100

    link with node

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 var express = require('express')
 var app = express()
+var exec = require('child_process').exec;
+
+function execute(command, callback){
+    exec(command, function(error, stdout, stderr){
+		callback(stdout)
+	})
+}
 
 app.get('/', function (req, res) {
 	res.send(JSON.stringify({}))
 })
 
-app.get('/sync', function (req, res) {
+app.post('/sync', function (req, res) {
+	console.log(req.body)
+	execute('./connect.sh',console.log)
 	res.send(JSON.stringify({}))
 })
 
